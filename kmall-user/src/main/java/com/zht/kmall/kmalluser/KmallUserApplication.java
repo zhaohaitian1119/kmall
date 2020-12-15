dcabcf5a652aeea24a1762501415b170bfda4dfb
zhaohaitian_glb <397408662@qq.com>
2020-12-15 10:28:53 +0800

    创建kmall-user

---
+++ kmall-user/src/main/java/com/zht/kmall/kmalluser/KmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.zht.kmall.kmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmallUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmallUserApplication.class, args);
+	}
+
+}
